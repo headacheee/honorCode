863b3255566097501a85cd705ed8ac6610ed8b78
headache <headache@MidhaeldeiMac.local>
2016-08-25 20:58:53 +1000

    T1

--- code/Run.java
+++ code/Run.java
@@ -1,4 +1,4 @@
-
+ 
 public class Run {
 	public static void main(String[] args){
 		long start = System.nanoTime();
--- code/Topology.java
+++ code/Topology.java
@@ -1,7 +1,26 @@
 //http://sndlib.zib.de/home.action
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
 
 public class Topology {
-	String vector[];
+	
+	public static void main(String[] args){
+		try{
+			File inputFile = new File("abilene.xml");
+			DocumentBuilderFactory dbFactory  = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			
+			Element e = (Element)doc.getDocumentElement().getElementsByTagName("node").item(0);
+
+			
+			System.out.println(e.getElementsByTagName("x").item(0).getTextContent());
+		}catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	}
 	
 	
 }
